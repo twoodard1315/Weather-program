dbb1a4893bc60bc6a4df92207df9196e557b9185
Tristan Woodard <tlw29679@email.vccs.edu>
2020-05-08 09:28:21 -0400

    Add project files.

---
+++ WeatherApp/App_Code/jsonParse.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace WeatherApp.App_Code
+{
+    public class jsonParse
+    {
+        static string apiKey = "pJF9LbDmPs9nMLRW0s09t1OxQTGgowa1";
+        static string city = "Blairs";
+        static string locationUrl = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=pJF9LbDmPs9nMLRW0s09t1OxQTGgowa1&q=blairs";
+        static string fiveDayForecastUrl = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/2219475?apikey=pJF9LbDmPs9nMLRW0s09t1OxQTGgowa1&q";
+        static string currentConditionsUrl = "http://dataservice.accuweather.com/currentconditions/v1/2219475?apikey=pJF9LbDmPs9nMLRW0s09t1OxQTGgowa1&q";
+        static string twelveHourForecastUrl = "http://dataservice.accuweather.com/forecasts/v1/hourly/12hour/2219475?apikey=pJF9LbDmPs9nMLRW0s09t1OxQTGgowa1&q";
+
+
+        public JArray parseLocation() {
+            WebClient client = new WebClient();
+            string locationData = client.DownloadString(locationUrl);
+
+            JArray location = JArray.Parse(locationData);
+            return location;
+        }        
+
+        public JArray parseFiveDay()
+        {
+            WebClient client = new WebClient();
+            string fiveDayData = client.DownloadString(fiveDayForecastUrl);
+
+            JArray fiveDay = JArray.Parse(fiveDayData);
+            return fiveDay;
+        }
+
+        public string parseCurrent()
+        {
+            string temp = String.Empty;
+            WebClient client = new WebClient();
+            string currentData = client.DownloadString(currentConditionsUrl);
+
+            JArray current = JArray.Parse(currentData);
+            temp = current[0]["Temperature"]["Imperial"]["Value"].ToString();
+            return temp;
+            //return current["Weather Text"];
+        }
+
+        public JArray parseTwelve()
+        {
+            WebClient client = new WebClient();
+            string twelveData = client.DownloadString(twelveHourForecastUrl);
+
+            JArray twelveHour = JArray.Parse(twelveData);
+            return twelveHour;
+        }
+    }
+}
---
+++ WeatherApp/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WeatherApp.App_Code;
+
+
+namespace WeatherApp
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            jsonParse test = new jsonParse();
+            //Response.Write(test.parseLocation());
+            //Response.Write(test.parseFiveDay());
+            
+
+            Response.Write(test.parseCurrent());
+            //Response.Write(test.parseTwelve());
+
+        }
+    }
+}
